1b271ba6d83b26de55b812f425822a8e25f78349
soe-j <soeda@val.co.jp>
2020-03-12 17:21:45 +0900

    fix to update NODE_ENV and FunctionName at command

--- bin/lamb.js
+++ bin/lamb.js
@@ -10,11 +10,22 @@ console.log('environment:', env);
 const lambda = new Lambda(funcFolderPath, env);
 const git = new Git();
 
+const functionName = (() => {
+  if (env === 'production') return lambda.config.FunctionName;
+  return `${lambda.config.FunctionName}-${env}`;
+})();
+
 (async () => {
   await git.isClean();
 
   await lambda.build();
   await lambda.create({
+    FunctionName: functionName,
+    Environment: {
+      Variables: {
+        NODE_ENV: "env"
+      }
+    },
     Tags: {
       Commit: await git.revparse('HEAD')
     }
