80655fe0ef5cb34d7dcce8ecf94681676e15fc16
soe-j <soeda@val.co.jp>
2020-03-12 20:35:23 +0900

    template lamb.config.js

---
+++ templates/lamb.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  FunctionName: "sayHelloWorld",
+  Description: "say hello world",
+  Handler: "index.handler",
+  Role: "arn:aws:iam::123456789012:role/awesome-role",
+  MemorySize: "128",
+  Timeout: "5",
+  Runtime: "nodejs12.x",
+  /** overwrite by lamb
+
+    Environment: {
+      Variables: {
+        NODE_ENV: "staging"
+      }
+    },
+    Tags: {
+      Commit: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    },
+  */
+  VpcConfig: {
+    SubnetIds: [
+      "subnet-xxxxxxxxxxxxxxxxx",
+      "subnet-yyyyyyyyyyyyyyyyy",
+      "subnet-zzzzzzzzzzzzzzzzz",
+    ],
+    SecurityGroupIds: [
+      "sg-xxxxxxxxxxxxxxxxx"
+    ]
+  },
+  EventInvokeConfig: {
+    MaximumEventAgeInSeconds: "60",
+    MaximumRetryAttempts: "0",
+  }
+}
